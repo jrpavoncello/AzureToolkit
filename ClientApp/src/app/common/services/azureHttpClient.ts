349497deac7ce536d8e9c4fd3cc78586878ec8f6
Josh Pavoncello <jrpavoncello@gmail.com>
2018-09-07 01:46:37 -0400

    Fixed computer vision and bing search api calls

--- ClientApp/src/app/common/services/azureHttpClient.ts
+++ ClientApp/src/app/common/services/azureHttpClient.ts
@@ -4,15 +4,15 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AzureHttpClient {
     constructor(private http: HttpClient) {}
     get(url: string, apiKey: string) {
-        let headers = new HttpHeaders();
-        headers.append('Ocp-Apim-Subscription-Key', apiKey);
+        let headers = new HttpHeaders()
+            .append('Ocp-Apim-Subscription-Key', apiKey);
         return this.http.get(url, {
             headers: headers
         });
     }
     post(url, apiKey, data) {
-        let headers = new HttpHeaders();
-        headers.append('Ocp-Apim-Subscription-Key', apiKey);
+        let headers = new HttpHeaders()
+            .append('Ocp-Apim-Subscription-Key', apiKey);
         return this.http.post(url, data, {
             headers: headers
         });
