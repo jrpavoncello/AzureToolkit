47fc000df7e703fa19b76fe1ae48a711ab210a7b
Josh Pavoncello <jrpavoncello@gmail.com>
2018-09-10 19:54:22 -0400

    Cleaned up some of the old template stuff

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { SearchComponent } from './search/search.component';
 import { DateService } from './date/date.service';
 import { CommonModule } from './common/common.module';
@@ -18,7 +17,6 @@ import { UserService } from './common/services/user.service';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    FetchDataComponent,
     SearchComponent
   ],
   imports: [
@@ -28,7 +26,6 @@ import { UserService } from './common/services/user.service';
     CommonModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'search', component: SearchComponent },
     ])
   ],
